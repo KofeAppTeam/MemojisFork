40a0360701fe8fe7a8e29e008b42f1d63ea5ca7e
Alex Kuznetcov <61458847+iamalexman@users.noreply.github.com>
2024-06-05 00:26:19 +0300

    Update build-docc.sh

--- build-docc.sh
+++ build-docc.sh
@@ -8,6 +8,6 @@ xcrun xcodebuild docbuild \
 xcrun docc process-archive transform-for-static-hosting \
 	"$PWD/.derivedData/Build/Products/Debug-iphonesimulator/Memojis.doccarchive" \
 	--output-path ".docs" \
-	--hosting-base-path "MemojisFork"
+	--hosting-base-path "kofeappteam"
 
 echo '<script>window.location.href += "/documentation/memojis"</script>' > .docs/index.html
